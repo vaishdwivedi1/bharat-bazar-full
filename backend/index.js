f4fbb9370a0fcc77d02488f1844a835577af7310
Vaishnavi <114218591+vaishdwivedi1@users.noreply.github.com>
2026-03-22 13:02:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,27 @@ connectDB();
 
 // app
 const app = express();
-app.use(cors());
 
 // middlewares
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cyberwolve"],
+    maxAge: 24 * 60 * 60 * 1000,
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api/auth", authRoutes);
